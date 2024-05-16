a5f9dc00a83b04561114f612196223fa476f1ea7
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-05-15 22:40:23 -0600

    Commit inicial

---
+++ Ejercicio_2-ImprimirPantalla.py
@@ -0,0 +1,8 @@
+print('Este es un mensaje')
+
+print('Este es un segundo mensaje.', end='')
+print('Este es un tercer mensaje.', end='')
+
+print()
+
+print('Este', 'es', 'un', 'cuarto', 'mensaje', sep='-')
