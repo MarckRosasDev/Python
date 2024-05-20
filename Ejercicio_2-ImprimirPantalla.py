a7cc0fcb03266b68b9e2c63d3062a919f981d4c6
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-05-20 12:15:50 -0600

    Adición de ejemplos en Ejericio 2

--- Ejercicio_2-ImprimirPantalla.py
+++ Ejercicio_2-ImprimirPantalla.py
@@ -10,3 +10,22 @@ print('Este', 'es', 'un', 'cuarto', 'mensaje', sep='-')
 print('Hola', 'mundo', 1.6, (1,2,3))
 
 print('{} poco a poco {}'.format('Aprendiendo', 'Python'))
+
+# Caracteres especiales \t  \n
+
+print('Este es un texto\tcon una tabulación')
+print('Este es un texto\ncon un salto de línea')
+print(r"C:\user\directorio") # r(cadena) ->raw(cruda)
+print("""Primera línea
+      segunda línea
+      tercera línea""")
+
+cadena='Primera línea\nSegunda línea'
+print(cadena)
+print(cadena+cadena)
+
+cadenas = 'Primera cadena' 'Segunda cadena'
+print(cadenas)
+
+cinco_espacios = ' '*5
+print(cinco_espacios+'Este texto tiene 5 espacios')
