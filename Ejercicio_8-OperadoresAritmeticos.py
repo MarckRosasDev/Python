d7ada34e687a263ad64cdad5185ea13f285a9774
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-05-24 10:16:19 -0600

    Adición del Ejercicio 8

---
+++ Ejercicio_8-OperadoresAritmeticos.py
@@ -0,0 +1,27 @@
+print('---SUMA---')
+print('34+55')
+print(34+55)
+
+print('---RESTA---')
+print('34-55')
+print(55-34)
+
+print('---MULTIPLICACIÓN---')
+print('34*55')
+print(34*55)
+
+print('---DIVISIÓN---')
+print('34/55')
+print(55/34)
+
+print('---POTENCIA---')
+print('2^6') 
+print(2**6) # 2*2*2*2*2*2
+
+print('---DIVISIÓN ENTERA---')
+print('55//34')
+print(55//34) # Nota: No es redondeo
+
+print('---MÓDULO---')
+print('55//55')
+print(55//34)
