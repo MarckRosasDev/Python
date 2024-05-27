8c02f0e93379b54708ab6002d32f7f37d821ae51
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-05-27 12:14:54 -0600

    Adición del Ejercicio 9

---
+++ Ejercicio_9-OperadoresAritméticosConVariables.py
@@ -0,0 +1,21 @@
+numero_1=100
+numero_2=30
+
+print(type(numero_1))
+print(type(numero_2))
+
+print(numero_1+numero_2)
+print(numero_1-numero_2)
+print(numero_1*numero_2)
+print(numero_1/numero_2)
+print(numero_1//numero_2)
+print(numero_1**numero_2)
+print(numero_1%numero_2)
+
+print('----------')
+
+suma = numero_1+numero_2
+print(suma)
+
+resta = numero_1-numero_2
+print(resta)
