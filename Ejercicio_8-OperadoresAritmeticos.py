f374902ef898723a0b9a5aab133b9a287bdbc65b
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-05-27 12:46:07 -0600

    Actualización del Ejercicio 8

--- Ejercicio_8-OperadoresAritmeticos.py
+++ Ejercicio_8-OperadoresAritmeticos.py
@@ -23,5 +23,48 @@ print('55//34')
 print(55//34) # Nota: No es redondeo
 
 print('---MÓDULO---')
-print('55//55')
-print(55//34)
+print('55%34')
+print(55%34)
+
+"""
+Recodatorio:
+1) Negación (-): Cambia el signo de un número.
+
+2) Módulo (%): Obtiene el residuo de la división entre dos números.
+El operador % se conoce como el operador módulo o residuo. Se utiliza para obtener el residuo de la división entera entre dos números.
+El operador módulo se utiliza de la siguiente manera:
+resultado = dividendo % divisor
+
+En Python, el operador % se conoce como el operador módulo o residuo. Se utiliza para obtener el residuo de la división entera entre dos números.
+
+Sintaxis:
+
+El operador módulo se utiliza de la siguiente manera:
+
+Python
+resultado = dividendo % divisor
+Usa el código con precaución.
+content_copy
+Donde:
+
+resultado es la variable que almacenará el residuo.
+dividendo es el número que se divide.
+divisor es el número por el que se divide.
+
+
+3) Potencia ():** Eleva un número a una potencia:
+La potencia se escribe de la siguiente manera:
+
+base ^ exponente = potencia
+
+Ejemplo:
+
+En la expresión 2 ^ 3, la base es 2 y el exponente es 3.
+
+Significado:
+
+La potencia 2 ^ 3 significa que se multiplica el número 2 por sí mismo 3 veces:
+
+2 ^ 3 = 2 * 2 * 2 = 8 
+    
+"""
