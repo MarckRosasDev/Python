db57117f4e96aaa2f4a00a806fc1d6b8bd51309b
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-05-30 23:48:27 -0600

    Adición del Ejercicio 14

---
+++ Ejercicio_14-OperadoresRelacionales.py
@@ -0,0 +1,39 @@
+"""
+OPERADOR    DESCRIPCIÓN
+>           Mayor que
+<           Menor que
+>=          Mayor o igual que
+<=          Menor o igual que
+==          Igual que
+!=          Diferente que
+"""
+print(100>90)
+print(100<90)
+print(100>=90)
+print(100<=90)
+print(100==90)
+print(100!=90)
+
+print('--------')
+numero_uno = 78
+numero_dos = 34
+
+resultado = numero_uno<numero_dos
+print(resultado)
+
+resultado = numero_uno>numero_dos
+print(resultado)
+
+resultado = numero_uno<=numero_dos
+print(resultado)
+
+resultado = numero_uno>=numero_dos
+print(resultado)
+
+resultado = numero_uno==numero_dos
+print(resultado)
+
+resultado = numero_uno!=numero_dos
+print(resultado)
+
+print('--------')
