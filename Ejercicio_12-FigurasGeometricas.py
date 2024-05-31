23408d7354072af062411453f98131b7af46b1c6
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-05-30 22:40:08 -0600

    Actualización del Ejercciio 12

--- Ejercicio_12-FigurasGeometricas.py
+++ Ejercicio_12-FigurasGeometricas.py
@@ -1,10 +1,10 @@
-#Calcula el área de un círculo
+# Calcula el área de un círculo
 
 import math
 
-print('ÁREA DE UN CÍRCULO')
+print("ÁREA DE UN CÍRCULO")
 radio = float(input("Ingrese el radio del círculo: "))
-area_circulo = math.pi * radio ** 2
+area_circulo = math.pi * radio**2
 print(f"El área del círculo es: {area_circulo:.2f} unidades cuadradas")
 """
 {area}: Esta es una expresión que se inserta dentro de la cadena de texto.
@@ -12,18 +12,21 @@ Se refiere a la variable area, que presumiblemente contiene el valor del área d
 
 :.2f: Este es un formateador de número que indica que el valor de la variable area debe
 imprimirse como un número decimal con dos dígitos después del punto decimal.
+La cantidad de dígitos decimales que se muestran después del punto decimal se puede ajustar
+modificando el formateador de número, por ejemplo, :.3f para tres decimales o :.4f para
+cuatro decimales.
 """
-print('---------')
+print("---------")
 
-#Calcula el área de un cuadrado
-print('ÁREA DE UN CUADRADO')
+# Calcula el área de un cuadrado
+print("ÁREA DE UN CUADRADO")
 lado = float(input("Ingrese la longitud de un lado del cuadrado: "))
-#area_cuadrado = lado ** 2
-area_cuadrado = pow(lado,2)
+# area_cuadrado = lado ** 2
+area_cuadrado = pow(lado, 2)
 print(f"El área del cuadrado es: {area_cuadrado:.2f} unidades cuadradas")
-print('---------')
+print("---------")
 
-#Calcula el área de un triángulo
+# Calcula el área de un triángulo
 base = float(input("Ingrese la base del triángulo: "))
 altura = float(input("Ingrese la altura del triángulo: "))
 area_triangulo = (base * altura) / 2
