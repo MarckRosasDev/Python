e9d81a16ee9516c166fadd1974fe1393731c67d7
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-06-03 23:28:46 -0600

    Acualización Ejercicio 17

--- Ejercicio_17-ExpresionesAnidadas.py
+++ Ejercicio_17-ExpresionesAnidadas.py
@@ -10,3 +10,35 @@ a = 20
 b = 10
 
 print(a * b - 2**b >= 20 and not (a % b) != 0)
+
+"""
+Explicación paso a paso:
+
+Separación de la expresión:
+La expresión se divide en dos partes unidas por el operador and:
+
+Primera parte: a * b - 2**b >= 20
+Segunda parte: not (a % b) != 0
+Análisis de la primera parte:
+
+Operación de multiplicación: a * b = 20 * 10 = 200
+Operación de potencia: 2**b = 2^10 = 1024
+Operación de resta: 200 - 1024 = -824
+Operación de comparación: -824 >= 20
+Resultado de la primera parte:
+
+La primera parte de la expresión, -824 >= 20, es falsa. Esto se debe a que el valor -824 es menor que 20.
+
+Análisis de la segunda parte:
+
+Operación de módulo: a % b = 20 % 10 = 0
+Negación: not (0) = True
+Comparación: True != 0
+Resultado de la segunda parte:
+
+La segunda parte de la expresión, not (a % b) != 0, es verdadera. Esto se debe a que el valor 0 es igual a 0, por lo que la negación (not) lo convierte en True, y la comparación != 0 evalúa como True ya que True es diferente de 0.
+
+Análisis de la expresión completa:
+
+Como la expresión se compone de dos partes unidas por el operador and, y la primera parte es falsa, la expresión completa es falsa.
+"""
