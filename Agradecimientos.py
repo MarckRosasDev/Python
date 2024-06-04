152078983595b22af78fbf76a232183643cd3726
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-06-04 13:31:12 -0600

    Adición de los Agradecimientos

---
+++ Agradecimientos.py
@@ -0,0 +1,16 @@
+print("Agradezco a los siguientes mentores que han compartido sus conocimientos")
+print()
+
+nombres = [
+    "mouredev",
+    "midudev",
+    "Alvaro Chirou",
+    "HolaMundoDev",
+    "Hektor Docs",
+    "pildorasinformaticas",
+    "John Ortiz Ordoñez",
+]
+
+
+for nombre in nombres:
+    print("- " + nombre)
