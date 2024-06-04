2cf012afc01b1300587f6c8578fee293aa73d0cf
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-06-04 13:35:20 -0600

    Adición del Ejercicio 18

---
+++ Ejercicio_18-OperadoresAsignacion.py
@@ -0,0 +1,41 @@
+# SUMA en asignación
+sum_a = 10
+print(sum_a)
+sum_a += 1
+print(sum_a)
+print()
+
+# RESTA en asignación
+rest_a = 10
+print(rest_a)
+rest_a -= 1
+print(rest_a)
+print()
+
+# PRODUCTO en asignación
+pro_a = 10
+print(pro_a)
+pro_a *= 2
+print(pro_a)
+print()
+
+# DIVISIÓN en asignación
+divi_a = 10
+print(divi_a)
+divi_a /= 2
+print(divi_a)
+print()
+
+# MÓDULO en asignación
+modu_a = 10
+print(modu_a)
+modu_a %= 3
+print(modu_a)
+print()
+
+# POTENCIA en asignación
+potencia = 10
+print(potencia)
+potencia **= 3
+print(potencia)
+print()
