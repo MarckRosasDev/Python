f0e498e7c06877999724f8872d7d7d70a5139581
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-06-11 00:43:19 -0600

    Actualización del Ejercicio 20

--- Ejercicio_20-Condicionales.py
+++ Ejercicio_20-Condicionales.py
@@ -14,7 +14,7 @@ if numero == 10:
     print("Número equivale a 10")
 print()
 
-print('--- Anidación ---')
+print("--- Anidación ---")
 # Anicación 1
 numero_1 = 20
 numero_2 = 30
@@ -26,16 +26,16 @@ if numero_1 == 20:
 print()
 
 # Anidacion 2
-nombre= 'Pepe'
+nombre = "Pepe"
 edad = 18
 
-if nombre == 'Pepe':
+if nombre == "Pepe":
     if edad >= 18:
-        print('Eres Pepe y ya puedes votar')
+        print("Eres Pepe y ya puedes votar")
     else:
-        print('Eres Pepe, pero no tienes edad para votar')
+        print("Eres Pepe, pero no tienes edad para votar")
 else:
-    print('Tu no eres Pepe')
+    print("Tu no eres Pepe")
 print()
 
 # Evaluación de múltiples expresiones
@@ -54,39 +54,39 @@ if numero % 2 == 0:
 else:
     print(f"{numero} es un número impar")
 print()
-print('--- elif ---')
-calificacion = float(input('Introduce tu calificación: '))
+print("--- elif ---")
+calificacion = float(input("Introduce tu calificación: "))
 if calificacion == 10:
-    print('Tu calificación es sobresaliente')
+    print("Tu calificación es sobresaliente")
 elif calificacion == 9:
-    print('Tu calificación es notable')
+    print("Tu calificación es notable")
 elif calificacion == 8:
-    print('Tu calificación es buena')
+    print("Tu calificación es buena")
 elif calificacion == 7:
-    print('Tu calificación es suficiente')
-elif calificacion <=6:
-    print('Tu calificación es insuficiente, no aprobaste')
+    print("Tu calificación es suficiente")
+elif calificacion <= 6:
+    print("Tu calificación es insuficiente, no aprobaste")
 else:
-    print('Calificación fuera de rango')
-    
+    print("Calificación fuera de rango")
+
 print()
 
 letra = input("Ingresa una letra: ")
-#print(type(letra))
-if letra.lower() == 'a':
-    print('Esta vocal es la a')
-elif letra.lower() == 'e':
-    print('Esta vocal es la e')
-elif letra.lower() == 'i':
-    print('Esta vocal es la i')
-elif letra.lower() == 'o':
-    print('Esta vocal es la o')
-elif letra.lower() == 'u':
-    print('Esta vocal es la u')
+# print(type(letra))
+if letra.lower() == "a":
+    print("Esta vocal es la a")
+elif letra.lower() == "e":
+    print("Esta vocal es la e")
+elif letra.lower() == "i":
+    print("Esta vocal es la i")
+elif letra.lower() == "o":
+    print("Esta vocal es la o")
+elif letra.lower() == "u":
+    print("Esta vocal es la u")
 else:
-    print('Tu letra es una consonante,un número o una cadena de caracteres')
+    print("Tu letra es una consonante,un número o una cadena de caracteres")
 print()
-print('--- pass ---')
-if True: 
-    pass # Sirve para utilizar en un bloque de código vacío, no finaliza el código
-    print('Continua el código...')
+print("--- pass ---")
+if True:
+    pass  # Sirve para utilizar en un bloque de código vacío, no finaliza el código
+    print("Continua el código...")
