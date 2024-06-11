248042a789f52ae9b18f78fc3f40d1bf92087ede
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-06-10 20:06:21 -0600

    Actualización del Ejercicio 19

--- Ejercicio_19-Strings_2.py
+++ Ejercicio_19-Strings_2.py
@@ -1,30 +1,30 @@
-palabra = 'Electronica'
+palabra = "Electronica"
 print(palabra[0])
 print(palabra[1])
 print(palabra[2])
 print()
-print(palabra[-1]) # Última posición de la cadena de caracteres
+print(palabra[-1])  # Última posición de la cadena de caracteres
 print(palabra[-2])
 print(palabra[-3])
 print()
-print('--- Slicing ---') 
-print(palabra[0:5]) # [Inicio:Fin]
-print(palabra[0:]) # [0: Fin]
+print("--- Slicing ---")
+print(palabra[0:5])  # [Inicio:Fin]
+print(palabra[0:])  # [0: Fin]
 print(palabra[2:])
 print(palabra[-2:])
-print(palabra[:2]) # [Inicio:2]
+print(palabra[:2])  # [Inicio:2]
 print(palabra[:-2])
 print(palabra[:])
-print(palabra[:2]+palabra[2:])
+print(palabra[:2] + palabra[2:])
 print()
-#print(palabra[58]) -> Error
-print('--- Inmutabilidad ---')
+# print(palabra[58]) -> Error
+print("--- Inmutabilidad ---")
 # palabra[0]=H -> Error
 # NO se puede modificar el contenido de una cadena
 # Sí se puede concatenar y/o utilizar slicing con otras cadenas o caracter
-palabra='e'+palabra[1:]
+palabra = "e" + palabra[1:]
 print(palabra)
 print()
-print('--- Longitud ---')
-palabra='Electrón'
+print("--- Longitud ---")
+palabra = "Electrón"
 print(len(palabra))
