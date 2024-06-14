9e4f3efd56f271794d2b33ceeb5d14e5dbf6c96e
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-06-14 12:07:35 -0600

    Actualización del Ejercicio 22

--- Ejercicio_22-PracticaLoAprendido.py
+++ Ejercicio_22-PracticaLoAprendido.py
@@ -21,25 +21,43 @@ print()
 
 print("--- Programa 3 ---")
 # Crea un programa que ingrese una letra e indique si es vocal o no es vocal
-letra = input('Ingresa una letra: ')
-if letra.lower() in 'aeiou':
-    print('La letra es una vocal')
+letra = input("Ingresa una letra: ")
+if letra.lower() in "aeiou":
+    print("La letra es una vocal")
 else:
-    print('La letra no es una vocal')
+    print("La letra no es una vocal")
 print()
 
 print("--- Programa 4 ---")
-num= int(input('Ingresa un número entero: '))
-if num>0:
-    print(f'El valor absoluto de {num} es: {num}')
+num = int(input("Ingresa un número entero: "))
+if num > 0:
+    print(f"El valor absoluto de {num} es: {num}")
 else:
-    print(f'El valor absoluto de {num} es: {abs(num)}')
+    print(f"El valor absoluto de {num} es: {abs(num)}")
 print()
 
 print("--- Programa 5 ---")
 # Utilizando opradores lógicos, determina si una cadena de texto tiene una longitud mayor o igual a 5 y menor o igual a 10
-cadena= input('Ingresa la cadena de texto: ')
-if len(cadena)>=5 or len(cadena) <=10:
-    print('La cadena es mayor que 5 caracteres pero menor que 10 caracteres.')
+cadena = input("Ingresa la cadena de texto: ")
+if len(cadena) >= 5 or len(cadena) <= 10:
+    print("La cadena es mayor que 5 caracteres pero menor que 10 caracteres.")
 else:
-    print('La cadena es menor de 5 caracteres o mayor de 10 caracteres.')
+    print("La cadena es menor de 5 caracteres o mayor de 10 caracteres.")
+print()
+
+print("--- Programa 6 ---")
+# Codifica un programa que solicite 2 palabras y ue indique si riman.
+# Condiciones: Si las 3 últimas letras coinciden se despliega: Riman.
+#  Si las 2 últimas letras coinciden se despliega: Riman un poco.
+#  Si no coinciden las últimas letras se despliega: No riman.
+palabra_1 = input("Ingresa la primera palabara: ")
+palabra_2 = input("Ingresa la segunda palabara: ")
+
+if len(palabra_1) < 3 or len(palabra_2) < 3:
+    print("Las palabras tienen menos de 3 caracteres, por lo tanto, no riman.")
+elif palabra_1[-3:] == palabra_2[-3:]:
+    print("Riman.")
+elif palabra_1[-2:] == palabra_2[-2:]:
+    print("Riman un poco.")
+else:
+    print('No riman.')
