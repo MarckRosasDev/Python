5ad030e4b213b7e554b1e010896ae15550bd362f
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-06-28 23:32:27 -0600

    Actualización del Ejercicio 25 parte 2

--- Ejercicio_25-Diccionarios_2.py
+++ Ejercicio_25-Diccionarios_2.py
@@ -1,4 +1,4 @@
-print('--- Métodos en diccionarios ---')
+print("--- Métodos en diccionarios ---")
 
 Caricatura = {
     "nombre": "Félix el Gato",
@@ -8,7 +8,7 @@ Caricatura = {
         "ojos": "Blancos y grandes",
         "boca": "Amplia con sonrisa pícara",
         "cola": "Larga y delgada",
-        "cuerpo": "Flexible y ágil"
+        "cuerpo": "Flexible y ágil",
     },
     "personalidad": {
         "travieso": True,
@@ -18,7 +18,7 @@ Caricatura = {
         "optimista": True,
         "alegre": True,
         "servicial": True,
-        "torpe": True
+        "torpe": True,
     },
     "habilidades": {
         "agilidad": "Alta",
@@ -26,49 +26,65 @@ Caricatura = {
         "trucos": True,
         "cola_herramienta": True,
         "bolsas_magicas": True,
-        "resolucion_problemas": "Creativa e ingeniosa"
+        "resolucion_problemas": "Creativa e ingeniosa",
     },
-    "frases_celebres": ["¡Ay, caramba!", "¡Miau!", "¡No puedo creerlo!", "¡Esto sí que es un lío!", "¡Estoy bien, estoy bien!"],
+    "frases_celebres": [
+        "¡Ay, caramba!",
+        "¡Miau!",
+        "¡No puedo creerlo!",
+        "¡Esto sí que es un lío!",
+        "¡Estoy bien, estoy bien!",
+    ],
     "relaciones": {
         "villanos": ["Profesor Wacko", "Poindexter"],
         "amigo_humano": "Dick Tracy",
-        "compañeros": "Otros gatos o animales"
-    }
+        "compañeros": "Otros gatos o animales",
+    },
 }
 print(Caricatura.keys())
 print(Caricatura.values())
 print()
-Caricatura['En emisión'] = False # Adicionar información al diccionario
+Caricatura["En emisión"] = False  # Adicionar información al diccionario
 print(Caricatura)
 print()
-pares= Caricatura.items() # Obtiene una vista de los pares clave-valor del diccionario
-print(f'Pares claves-valor: {pares}')
+pares = Caricatura.items()  # Obtiene una vista de los pares clave-valor del diccionario
+print(f"Pares claves-valor: {pares}")
 print()
-nombre=Caricatura.get('nombre', 'No existe') 
-print(f'El nombre del gato es: {nombre}')
+nombre = Caricatura.get("nombre", "No existe")
+print(f"El nombre del gato es: {nombre}")
 print()
-existe_nombre = 'nombre' in Caricatura # Verifica si una clave específica existe en el diccionario.
-existe_nombre_1 = 'Nombre' in Caricatura
+existe_nombre = (
+    "nombre" in Caricatura
+)  # Verifica si una clave específica existe en el diccionario.
+existe_nombre_1 = "Nombre" in Caricatura
 print(f'Existe la clave "nombre": {existe_nombre}')
 print(f'Existe la clave "Nombre": {existe_nombre_1}')
 print()
 diccionario = {"nombre": "Bit", "edad": 30}
 datos_nuevos = {"ciudad": "CDMX", "ocupacion": "Programador"}
-diccionario.update(datos_nuevos) # Actualiza el diccionario con pares clave-valor de otro diccionario o un objeto
+diccionario.update(
+    datos_nuevos
+)  # Actualiza el diccionario con pares clave-valor de otro diccionario o un objeto
 print(f"Diccionario actualizado: {diccionario}")
 print()
-ciudad = diccionario.pop("ciudad") # Elimina un elemento del diccionario por su clave y devuelve el valor asociado.
+ciudad = diccionario.pop(
+    "ciudad"
+)  # Elimina un elemento del diccionario por su clave y devuelve el valor asociado.
 print(f"Ciudad eliminada: {ciudad}")
 print(f"Diccionario actualizado: {diccionario}")
 print()
-diccionario.clear() # Elimina todos los elementos del diccionario.
+diccionario.clear()  # Elimina todos los elementos del diccionario.
 print(f"Diccionario después de limpiar: {diccionario}")
 print()
-cantidad_elementos = len(Caricatura) # Devuelve la cantidad de elementos (pares clave-valor) en el diccionario.
+cantidad_elementos = len(
+    Caricatura
+)  # Devuelve la cantidad de elementos (pares clave-valor) en el diccionario.
 print(f"Cantidad de elementos en el diccionario: {cantidad_elementos}")
 print()
 diccionario = {"nombre": "Juan", "edad": 30, "ciudad": "Ciudad de México"}
-copia_diccionario = diccionario.copy() # Crea una copia superficial del diccionario. Los cambios en la copia no afectan al original.
+copia_diccionario = (
+    diccionario.copy()
+)  # Crea una copia superficial del diccionario. Los cambios en la copia no afectan al original.
 copia_diccionario["ciudad"] = "Guadalajara"
 print(f"Diccionario original: {diccionario}")
 print(f"Copia del diccionario: {copia_diccionario}")
