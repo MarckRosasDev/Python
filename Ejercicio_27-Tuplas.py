22ebca05a75c7c2cf955050444a7f8d3e019c765
Marco Antonio Olmos <marck_rosas@hotmail.com>
2024-07-15 12:48:27 -0600

    Actualización del ejercicio 27

--- Ejercicio_27-Tuplas.py
+++ Ejercicio_27-Tuplas.py
@@ -1,4 +1,6 @@
-tupla = ('Python', 3.1416, 'Silicio', -88.9)
+# Colecciones ordenadas e inmutables de elementos de cualquier tipo.
+# Se utilizan para almacenar datos que no se van a modificar.
+tupla = ("Python", 3.1416, "Silicio", -88.9)
 print(tupla)
 print(type(tupla))
 print()
@@ -7,11 +9,11 @@ print(tupla[-1])
 print(tupla[1:])
 print(tupla[:2])
 print()
-#tupla[0] = 100 # Es inmutable
-print(tupla.index('Silicio')) # Busca un elemento y devuelve su posición en la tupla
-#print(tupla.index('silicio'))
+# tupla[0] = 100 # Es inmutable
+print(tupla.index("Silicio"))  # Busca un elemento y devuelve su posición en la tupla
+# print(tupla.index('silicio'))
 print()
-print(tupla.count('Python')) # Cuenta cuantas veces aparece un elemento en la tupla
-print(tupla.count('Electrón'))
+print(tupla.count("Python"))  # Cuenta cuantas veces aparece un elemento en la tupla
+print(tupla.count("Electrón"))
 print()
-#tupla.append(22.89) # Las tuplas son inmutables,no es posible modificar su contenido
+# tupla.append(22.89) # Las tuplas son inmutables,no es posible modificar su contenido
